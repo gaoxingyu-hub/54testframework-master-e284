3f3ce5347e1b913cf251439ad5949874bcd0882c
gaoxingyu <gaoxingyu@example.com>
2020-11-04 22:54:13 +0800

    mw1500改变控件的大小，以使长法文清楚

--- modules/mw1500_device/Ui_TEST1.py
+++ modules/mw1500_device/Ui_TEST1.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'Ui_TEST1.ui'
+# Form implementation generated from reading ui file 'Ui_TEST1.UI'
 #
 # Created by: PyQt5 UI code generator 5.15.1
 #
@@ -9,14 +9,13 @@
 
 
 from PyQt5 import QtCore, QtGui, QtWidgets
-
 from modules.mw1500_device.mw1500_constant import ModuleConstants
 
 
 class Ui_Dialog(object):
     def setupUi(self, Dialog):
         Dialog.setObjectName("Dialog")
-        Dialog.resize(600, 600)
+        Dialog.resize(871, 600)
         Dialog.setStyleSheet("QDialog{\n"
 "background-color:#E3EAF4;\n"
 "margin-top:10px;\n"
@@ -40,7 +39,7 @@ class Ui_Dialog(object):
 "font-family:Microsoft YaHei;\n"
 "}")
         self.groupBox = QtWidgets.QGroupBox(Dialog)
-        self.groupBox.setGeometry(QtCore.QRect(9, 9, 582, 582))
+        self.groupBox.setGeometry(QtCore.QRect(9, 9, 851, 582))
         font = QtGui.QFont()
         font.setFamily("Microsoft YaHei")
         font.setPointSize(-1)
@@ -51,7 +50,7 @@ class Ui_Dialog(object):
         self.groupBox.setCheckable(False)
         self.groupBox.setObjectName("groupBox")
         self.textBrowser_contents = QtWidgets.QTextBrowser(self.groupBox)
-        self.textBrowser_contents.setGeometry(QtCore.QRect(11, 1, 560, 69))
+        self.textBrowser_contents.setGeometry(QtCore.QRect(11, 1, 831, 69))
         self.textBrowser_contents.setStyleSheet("QTextBrowser{\n"
 "border-width:0;\n"
 "border-style:outset;\n"
@@ -66,18 +65,18 @@ class Ui_Dialog(object):
         self.groupBox_2.setTitle("")
         self.groupBox_2.setObjectName("groupBox_2")
         self.pushButton_2 = QtWidgets.QPushButton(self.groupBox)
-        self.pushButton_2.setGeometry(QtCore.QRect(320, 540, 75, 30))
+        self.pushButton_2.setGeometry(QtCore.QRect(550, 530, 85, 40))
         self.pushButton_2.setObjectName("pushButton_2")
         self.pushButton_1 = QtWidgets.QPushButton(self.groupBox)
-        self.pushButton_1.setGeometry(QtCore.QRect(190, 540, 75, 30))
+        self.pushButton_1.setGeometry(QtCore.QRect(240, 530, 85, 40))
         self.pushButton_1.setLayoutDirection(QtCore.Qt.LeftToRight)
         self.pushButton_1.setObjectName("pushButton_1")
         self.label_img = QtWidgets.QLabel(self.groupBox)
-        self.label_img.setGeometry(QtCore.QRect(10, 120, 560, 311))
+        self.label_img.setGeometry(QtCore.QRect(10, 120, 831, 311))
         self.label_img.setText("")
         self.label_img.setObjectName("label_img")
         self.label_1 = QtWidgets.QLabel(self.groupBox)
-        self.label_1.setGeometry(QtCore.QRect(30, 460, 60, 20))
+        self.label_1.setGeometry(QtCore.QRect(70, 460, 260, 20))
         font = QtGui.QFont()
         font.setFamily("Microsoft YaHei")
         font.setPointSize(-1)
@@ -85,10 +84,10 @@ class Ui_Dialog(object):
         self.label_1.setAlignment(QtCore.Qt.AlignCenter)
         self.label_1.setObjectName("label_1")
         self.lineEdit_1 = QtWidgets.QLineEdit(self.groupBox)
-        self.lineEdit_1.setGeometry(QtCore.QRect(100, 460, 100, 20))
+        self.lineEdit_1.setGeometry(QtCore.QRect(330, 460, 40, 20))
         self.lineEdit_1.setObjectName("lineEdit_1")
         self.comboBox_1 = QtWidgets.QComboBox(self.groupBox)
-        self.comboBox_1.setGeometry(QtCore.QRect(210, 460, 50, 20))
+        self.comboBox_1.setGeometry(QtCore.QRect(370, 460, 50, 20))
         font = QtGui.QFont()
         font.setFamily("Arial")
         font.setPointSize(8)
@@ -96,14 +95,14 @@ class Ui_Dialog(object):
         self.comboBox_1.setObjectName("comboBox_1")
         self.comboBox_1.addItem("")
         self.label_2 = QtWidgets.QLabel(self.groupBox)
-        self.label_2.setGeometry(QtCore.QRect(280, 460, 60, 20))
+        self.label_2.setGeometry(QtCore.QRect(430, 460, 260, 21))
         self.label_2.setAlignment(QtCore.Qt.AlignCenter)
         self.label_2.setObjectName("label_2")
         self.lineEdit_2 = QtWidgets.QLineEdit(self.groupBox)
-        self.lineEdit_2.setGeometry(QtCore.QRect(350, 460, 100, 20))
+        self.lineEdit_2.setGeometry(QtCore.QRect(690, 460, 40, 20))
         self.lineEdit_2.setObjectName("lineEdit_2")
         self.comboBox_2 = QtWidgets.QComboBox(self.groupBox)
-        self.comboBox_2.setGeometry(QtCore.QRect(460, 460, 50, 20))
+        self.comboBox_2.setGeometry(QtCore.QRect(730, 460, 50, 20))
         font = QtGui.QFont()
         font.setFamily("Arial")
         font.setPointSize(8)
@@ -123,3 +122,14 @@ class Ui_Dialog(object):
         self.comboBox_1.setItemText(0, _translate("Dialog", "MHz"))
         self.label_2.setText(_translate("Dialog", ModuleConstants.recivice_frequence))
         self.comboBox_2.setItemText(0, _translate("Dialog", "MHz"))
+import res.iconQrc_rc
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    Dialog = QtWidgets.QDialog()
+    ui = Ui_Dialog()
+    ui.setupUi(Dialog)
+    Dialog.show()
+    sys.exit(app.exec_())
+
+
